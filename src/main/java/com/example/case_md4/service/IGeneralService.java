17ed8fbae5c8aea23f5355e0d2e0176cc1c3ebf9
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-07-10 22:36:45 +0700

    toi thu 2

--- src/main/java/com/example/case_md4/service/IGeneralService.java
+++ src/main/java/com/example/case_md4/service/IGeneralService.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 public interface IGeneralService<E> {
     Iterable<E> findAll();
-    Page<E> findAll(Pageable pageable);
     Optional<E> findOne(Long id);
     E save(E e);
     void remove(Long id);
